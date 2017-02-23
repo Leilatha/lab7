0ad46138a1c51e226f6654d42692aa49855d7ee1
Marian <mcrofts@ucsd.edu>
2017-02-23 12:00:08 -0800

    almost done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var projects = require('../projects.json');
 
-/*
- * GET home page.
- */
-
 exports.view = function(req, res){
-  	res.render('index', projects);
+  	projects['min'] = false;
+   res.render('index', projects);
+ };
+ 
+ exports.viewMin = function(req, res){
+   projects['min'] = true;
+   res.render('index', projects);
 };
